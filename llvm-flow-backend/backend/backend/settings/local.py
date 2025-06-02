a8cbacfbca59ba1a259cb665db6427b6239753e8
Uiseop Eom <ytrtef@gmail.com>
2025-06-02 18:30:30 +0900

    [CHORE] Remove previous backend code

--- llvm-flow-backend/backend/backend/settings/local.py
+++
@@ -1,5 +0,0 @@
-from .base import *
-
-ALLOWED_HOSTS = ['*']
-
-DEBUG = True
--- llvm-flow-backend/backend/utils/datetime.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-from django.utils import timezone
-
-
-def get_now() -> datetime:
-    return timezone.now()
